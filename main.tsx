f5b65c971d1e5a6658586fab4c7d933ba3ffbf75
Fonhuss <fonhuss@gmail.com>
2025-08-10 18:36:37 +0200

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Planlæggeren from "./planlæggeren";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Planlæggeren />
+  </React.StrictMode>
+);
